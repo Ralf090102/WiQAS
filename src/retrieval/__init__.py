460978fe3150d226f4df4968df73cf329e695de0
Ralf090102 <ralf.hernandez090102@gmail.com>
2026-02-01 13:44:06 +0800

    import fix

--- src/retrieval/__init__.py
+++ src/retrieval/__init__.py
@@ -4,20 +4,23 @@ Retrieval module for WiQAS
 Contains components for document retrieval, reranking, and query processing.
 """
 
-from src.retrieval.embeddings import Embeddings
+from src.retrieval.embeddings import EmbeddingManager
 from src.retrieval.fusion import reciprocal_rank_fusion, weighted_fusion
 from src.retrieval.query_decomposer import QueryDecomposer
-from src.retrieval.reranker import Reranker
-from src.retrieval.retriever import Retriever
-from src.retrieval.search import Search
-from src.retrieval.vector_store import VectorStore
+from src.retrieval.reranker import RerankerManager
+from src.retrieval.retriever import WiQASRetriever
+from src.retrieval.search import SemanticSearcher, KeywordSearcher, HybridSearcher, MMRSearcher
+from src.retrieval.vector_store import ChromaVectorStore
 
 __all__ = [
-    "Embeddings",
-    "Reranker",
-    "Retriever",
-    "Search",
-    "VectorStore",
+    "EmbeddingManager",
+    "RerankerManager",
+    "WiQASRetriever",
+    "SemanticSearcher",
+    "KeywordSearcher",
+    "HybridSearcher",
+    "MMRSearcher",
+    "ChromaVectorStore",
     "QueryDecomposer",
     "reciprocal_rank_fusion",
     "weighted_fusion",
